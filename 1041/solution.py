807b3323ac7531a0442933a9309a3cd33b4189ab
Shashwat Shekhar <svs6603@psu.edu>
2020-11-06 20:38:08 +0530

    Complete challenge

--- 1041/solution.py
+++ 1041/solution.py
@@ -4,14 +4,43 @@ class Solution(object):
         :type instructions: str
         :rtype: bool
         """
-        direction = 1
-       
-        # 1: North
-        # 2: East
-        # 3: West
-        # 4: South
+        initial_x = 0
+        initial_y = 0
+        
+        initial_direction = "N"
 
+        for instruction in instructions:
 
+            if(instruction == "L"):
+                if(initial_direction == "N"):
+                    initial_direction = "W"
+                elif(initial_direction == "W"):
+                    initial_direction = "S"
+                elif(initial_direction == "E"):
+                    initial_direction = "N"
+                elif(initial_direction == "S"):
+                    initial_direction = "E"
+        
+            elif(instruction == "R"):
+                if(initial_direction == "N"):
+                    initial_direction = "E"
+                elif(initial_direction == "W"):
+                    initial_direction = "N"
+                elif(initial_direction == "E"):
+                    initial_direction = "S"
+                elif(initial_direction == "S"):
+                    initial_direction = "W"
+                    
+            elif(instruction == "G"):
+                if(initial_direction == "N"):
+                    initial_y += 1
+                elif(initial_direction == "W"):
+                    initial_x -= 1
+                elif(initial_direction == "E"):
+                    initial_x += 1
+                elif(initial_direction == "S"):
+                    initial_y -= 1
             
-            
-
+        if(initial_direction != "N" or (initial_x == 0 and initial_y == 0)):
+           return True
+        return False
