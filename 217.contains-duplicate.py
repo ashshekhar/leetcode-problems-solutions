c87fb5dac3e2f238560aee0dba69955fbd4d0824
Shashwat Shekhar <svs6603@psu.edu>
2020-12-22 18:03:36 +0530

    Pass 18/18 test cases

---
+++ 217.contains-duplicate.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=217 lang=python
+#
+# [217] Contains Duplicate
+#
+
+# @lc code=start
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        if(len(set(nums)) != len(nums)):
+            return True
+        return False
+          
+# @lc code=end
+
---
+++ 342.power-of-four.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=342 lang=python
+#
+# [342] Power of Four
+#
+
+# @lc code=start
+class Solution(object):
+    def isPowerOfFour(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        
+# @lc code=end
+
