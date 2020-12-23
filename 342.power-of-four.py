2f4653e65cb51970c204c8940e983ce35dffc967
Shashwat Shekhar <svs6603@psu.edu>
2020-12-24 00:36:17 +0530

    Pass 1047/1061 test cases

--- 342.power-of-four.py
+++ 342.power-of-four.py
@@ -11,6 +11,22 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
-        return type(pow(n, 1/4)) == int
+        float(n)
+
+        if(n == 1):
+            return True
+
+        if(n%4 != 0):
+            return False
+        
+        while type(n) is int:
+            n = float(n/4)
+            
+            if(n<=0):
+                return False
+            if(n == 1):
+                return True
+
+        return False
 # @lc code=end
 
