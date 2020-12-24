2d7124c48e66b560f26bdf23eb11a8fc5b5c3f21
Shashwat Shekhar <svs6603@psu.edu>
2020-12-24 16:10:13 +0530

    Pass 22/23 with queue implementation

--- 219.contains-duplicate-ii.py
+++ 219.contains-duplicate-ii.py
@@ -5,6 +5,8 @@
 #
 
 # @lc code=start
+from collections import deque
+
 class Solution(object):
     def containsNearbyDuplicate(self, nums, k):
         """
@@ -12,9 +14,26 @@ class Solution(object):
         :type k: int
         :rtype: bool
         """
+        k_nearby = deque()
+        
+        for i in range(1, k+1):
+            if(k < len(nums)):
+                k_nearby.append(nums[i])
+
+        if (not k_nearby and k>=len(nums)):
+            for i in range(1, len(nums)):
+                k_nearby.append(nums[i])
+
         for i in range(len(nums)):
-            if(nums[i] in set(nums[i+1:i+k+1])):
+            if(nums[i] in k_nearby):
                 return True
+            else:
+                if(k_nearby):
+                    k_nearby.popleft()
+                else:
+                    return False
+                if(i+k+1 < len(nums)):
+                    k_nearby.append(nums[i+k+1])
         return False
 # @lc code=end
 
