99406e909f8fb87c2273f8e04c5a0643bae37e7c
ashshekhar <svs6603@psu.edu>
2022-06-08 18:09:59 +0530

    Finish problems

---
+++ 283.move-zeroes.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode id=283 lang=python
+#
+# [283] Move Zeroes
+#
+
+# @lc code=start
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        # You don't have to shift every 0, just swap the first 0 and non-zero element once
+        length = len(nums)
+        
+        if length == 1:
+            return nums
+
+        current_0_pos = 0
+        
+        for i in range(length):
+            if nums[i] != 0:
+                nums[current_0_pos], nums[i] = nums[i], nums[current_0_pos]
+                current_0_pos += 1
+            
+# @lc code=end
+
