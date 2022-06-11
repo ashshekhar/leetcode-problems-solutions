e5804d3135b7a5cb50b21134d73c26c4584c4bad
ashshekhar <svs6603@psu.edu>
2022-06-12 00:06:21 +0530

    Finish problems

---
+++ 876.middle-of-the-linked-list.py
@@ -0,0 +1,42 @@
+#
+# @lc app=leetcode id=876 lang=python
+#
+# [876] Middle of the Linked List
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def middleNode(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        nodes = []
+        current = head
+        index = 0
+        
+        while current:
+            nodes.append(index)
+            index += 1
+            current = current.next
+        
+        mid_index = len(nodes) // 2
+        
+        current = head
+        new_index = 0
+
+        while new_index <= mid_index and current:
+            if new_index == mid_index:
+                return current
+            else:
+                current = current.next
+            new_index += 1
+            
+                 
+# @lc code=end
+
