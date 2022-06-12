1bf3aa849bef5bcf850d5782b96fabd8e98e3a03
ashshekhar <svs6603@psu.edu>
2022-06-12 17:39:42 +0530

    Finish problems

---
+++ 14.longest-common-prefix.py
@@ -0,0 +1,44 @@
+#
+# @lc app=leetcode id=14 lang=python
+#
+# [14] Longest Common Prefix
+#
+
+# @lc code=start
+from collections import deque
+
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        length = len(strs)
+        i = 0
+
+        if length == 0:
+            return ""
+        
+        if length == 1:
+            return strs[0]
+        
+        strs = sorted(strs, key=lambda x: len(x))
+        smallest = strs[0]
+        
+        while len(smallest) > 1:
+            for i in range(1, len(strs)):
+                if smallest in strs[i][:len(smallest)]:
+                    flag = True
+                else:
+                    flag = False
+                    break
+                
+            if flag:
+                return smallest if len(strs) > 0 else ""
+            else:  
+                smallest = smallest[:len(smallest)-1]
+            
+        return ""
+        
+# @lc code=end
+
