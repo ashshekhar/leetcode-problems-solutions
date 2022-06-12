531da50d514104a304e0bac4f782896573d5ffc4
ashshekhar <svs6603@psu.edu>
2022-06-13 00:25:08 +0530

    Finish problems

--- 79.word-search.py
+++ 79.word-search.py
@@ -6,40 +6,45 @@
 
 # @lc code=start
 class Solution(object):
+    def dfs(self, board, word, r, c, row, cols, index):
+        if r < 0 or c < 0 or r > row - 1 or c > cols - 1:
+            return
+        
+        char = board[r][c]
+        if char != word[index]:
+            return 
+        
+        if index == len(word) - 1:
+            return True
+        
+        directions = [[0, -1], [0, 1], [-1, 0], [1, 0]]
+        
+        board[r][c] = "V"
+        
+        for direction in directions:
+            if self.dfs(board, word, r+direction[0], c+direction[1], row, cols, index+1):
+                return True
+
+        board[r][c] = char
+        
+        return False
+            
+            
+            
     def exist(self, board, word):
         """
         :type board: List[List[str]]
         :type word: str
         :rtype: bool
         """
-        # Convert 2d matrix to graph adjacency dictionary
-        # Use of backtracking
+        rows = len(board)
+        cols = len(board[0])
         
-        rows, columns = len(board), len(board[0])
-        path = set()
-        
-        # i is the index of the character we are looking for in target word
-        def dfs(row, column, i):
-            
-            if i == len(word):
-                return True
-            
-            if (row < 0 or column < 0 or row >= rows or column >= columns) or (word[i] != board[row][column]) or (row, column) in path:
-                return False
-            
-            # We found our current character
-            path.add((row, column))
-            
-            # Else we found our current char, move on the next character
-            result =  dfs(row+1, column, i+1) or dfs(row, column+1, i+1) or dfs(row-1, column, i+1) or dfs(row, column-1, i+1)
-            
-            path.remove((row, column))
-            return result
-        
-        # Calling the recursive dfs on every cell
         for i in range(rows):
-            for j in range(columns):
-                if dfs(i, j, 0): return True
+            for j in range(cols):
+                if board[i][j] == word[0]:
+                    if self.dfs(board, word, i, j, rows, cols, 0):
+                        return True
         
         return False
 # @lc code=end
