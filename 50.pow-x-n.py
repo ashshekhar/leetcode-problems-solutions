f64e6c0693212e4738025d2e1487ba83911bb8b9
ashshekhar <svs6603@psu.edu>
2022-06-22 20:05:35 +0530

    Finish problems

--- 50.pow-x-n.py
+++ 50.pow-x-n.py
@@ -6,24 +6,77 @@
 
 # @lc code=start
 class Solution(object):
+    def helper(self, x, n):
+        if n == 0:
+            return 1
+
+        elif n == -1:
+            return 1/x
+        
+        if x == 1:
+            return x
+        
+        elif x == 0:
+            return 0
+        
+        result = self.helper(x, n//2)
+        result = result * result
+        return result if n%2 == 0 else x * result
+            
+        
     def myPow(self, x, n):
         """
         :type x: float
         :type n: int
         :rtype: float
         """
-        if(n == 1):
-            return x
+        # Better Approach
+        # 2^10 = (2^5) * (2^5) and keep breaking it down
+        neg = False
+        
+        if n < 0:
+            neg = True
+    
+        n = abs(n)
+        
+        res = self.helper(x, n)
 
-        elif (n == 0):
-            return 1
+        if neg:
+            return 1/res
+        return res
+    
+        # Basic Approach: Passes 301/305 test case; TLE in rest
+        # neg = False
             
-        elif(abs(n)>0):
-            output = x * self.myPow(x, abs(n)-1)
+        # if n < 0:
+        #     neg = True
+            
+        # elif n == 1:
+        #     return x
 
-        if(n<0):
-            return 1/output
-        return output
+        # elif n == 0:
+        #     return 1
+
+        # elif n == -1:
+        #     return 1/x
+        
+        # if x == 1:
+        #     return x
+
+        # n = abs(n)
+        # res = 1
+        
+        # while n >= 1:
+        #     res *= x
+        #     n -= 1
             
+        #     if res == 0:
+        #         return res
+
+        # if neg:
+        #     return 1/res
+        # return res
+    
+    
 # @lc code=end
 
