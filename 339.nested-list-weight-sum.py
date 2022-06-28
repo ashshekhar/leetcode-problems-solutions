c2d973a84c587737a01571c09e63cb093cacf168
ashshekhar <svs6603@psu.edu>
2022-06-28 14:46:49 +0530

    Finish problems

--- 339.nested-list-weight-sum.py
+++ 339.nested-list-weight-sum.py
@@ -2,33 +2,35 @@
 # This is the interface that allows for creating nested lists.
 # You should not implement it, or speculate about its implementation
 # """
-#class NestedInteger(object):
-#    def __init__(self, value=None):
-#        """
-#        If value is not specified, initializes an empty list.
-#        Otherwise initializes a single integer equal to value.
-#        """
-#
-#    def isInteger(self):
-#        """
-#        @return True if this NestedInteger holds a single integer, rather than a nested list.
-#        :rtype bool
-#        """
-#
-#    def getInteger(self):
-#        """
-#        @return the single integer that this NestedInteger holds, if it holds a single integer
-#        Return None if this NestedInteger holds a nested list
-#        :rtype int
-#        """
-#
-#    def getList(self):
-#        """
-#        @return the nested list that this NestedInteger holds, if it holds a nested list
-#        Return None if this NestedInteger holds a single integer
-#        :rtype List[NestedInteger]
-#        """
+       
 from collections import deque
+
+class NestedInteger(object):
+   def __init__(self, value=None):
+       """
+       If value is not specified, initializes an empty list.
+       Otherwise initializes a single integer equal to value.
+       """
+
+   def isInteger(self):
+       """
+       @return True if this NestedInteger holds a single integer, rather than a nested list.
+       :rtype bool
+       """
+
+   def getInteger(self):
+       """
+       @return the single integer that this NestedInteger holds, if it holds a single integer
+       Return None if this NestedInteger holds a nested list
+       :rtype int
+       """
+
+   def getList(self):
+       """
+       @return the nested list that this NestedInteger holds, if it holds a nested list
+       Return None if this NestedInteger holds a single integer
+       :rtype List[NestedInteger]
+       """
 
 class Solution(object):
     def depthSum(self, nestedList):
@@ -53,7 +55,9 @@ class Solution(object):
                 if item.isInteger():
                     res += depth * item.getInteger()
                 
-                # If the item is a list, then extract the integers and append to the end of the list
+                # If the item is a list, then append to the end of the list
+                # extend is used to "extend" the queue with the numbers inside the item
+                # extend takes in an iterator and appends the items in that to end
                 else:
                     queue.extend(item.getList())
             
