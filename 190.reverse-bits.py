fdc32b1f5b518cdd6c7515086c548653ea9adc33
ashshekhar <svs6603@psu.edu>
2022-06-28 21:41:11 +0530

    Finish problems

---
+++ 190.reverse-bits.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=190 lang=python
+#
+# [190] Reverse Bits
+#
+
+# @lc code=start
+class Solution:
+    # @param n, an integer
+    # @return an integer
+    def reverseBits(self, n):
+        res = 0
+        
+        # Since 32 bits
+        for _ in range(32):
+            res = res << 1
+            
+            # If even, last digit = bit = 0, else 1
+            bit = n % 2
+            
+            res += bit
+            
+            n = n >> 1
+        
+        return res
+            
+        
+# @lc code=end
+
