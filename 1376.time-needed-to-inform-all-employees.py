16788e6d3d804c302bfb0fb94065eb5a1fa47fbc
ashshekhar <svs6603@psu.edu>
2022-06-28 17:44:37 +0530

    Finish problems

---
+++ 1376.time-needed-to-inform-all-employees.py
@@ -0,0 +1,65 @@
+#
+# @lc app=leetcode id=1376 lang=python
+#
+# [1376] Time Needed to Inform All Employees
+#
+
+# @lc code=start
+class Solution(object):
+
+    def dfs(self, res, time, node, visited, manager_to_subordinate):
+        
+        # Base case: Only inform time stores
+        if not len(manager_to_subordinate):
+            return 0
+
+        # Base case
+        if len(manager_to_subordinate[node]) == 1:
+            return time
+
+        time += manager_to_subordinate[node][0]
+
+        for subordinates in manager_to_subordinate[node][1:]:
+            if subordinates not in visited:
+                visited.add(subordinates)
+                
+                # For each stage, you need to store the max ever encountered
+                res = max(res, self.dfs(res, time, subordinates, visited, manager_to_subordinate))
+        
+        return res
+
+    def numOfMinutes(self, n, headID, manager, informTime):
+        """
+        :type n: int
+        :type headID: int
+        :type manager: List[int]
+        :type informTime: List[int]
+        :rtype: int
+        """
+        # Key is n value of the employee, value is (time to infrom, [list of subordinates])
+        # Use of backtracking - Similar to course schedule
+        manager_to_subordinate = {}
+        
+        # time = 0
+        visited = set()
+        
+        # Creating dictionary mapping
+        for subordinate, manager in enumerate(manager):
+            if manager == -1:
+                continue
+                
+            if manager not in manager_to_subordinate:
+                manager_to_subordinate[manager] = [informTime[manager]]
+                
+            manager_to_subordinate[manager].append(subordinate)
+            
+            # For the rest of the subordinates
+            if subordinate not in manager_to_subordinate:
+                manager_to_subordinate[subordinate] = [informTime[subordinate]]
+                
+        res = self.dfs(0, 0, headID, visited, manager_to_subordinate)
+            
+        return res 
+# @lc code=end
+
+
--- 207.course-schedule.py
+++ 207.course-schedule.py
@@ -56,34 +56,3 @@ class Solution(object):
             
         
 # @lc code=end
-
-
-
-class Solution():
-  def __init__(self):
-    pass
-
-  def assignAndPrint(self,t):
-    d = dict()
-    for pair in t:
-      if(pair[0]==pair[1]): # because we dont want to assign self managing role
-        continue
-      if pair[0] not in d: # assign employee a empty list of employees
-        d[pair[0]] = []
-      # for managers -
-      if pair[1] not in d:
-        d[pair[1]] = [pair[0]]
-      else:
-        d[pair[1]].append(pair[0])
-
-    # now we know how many employees are directly under a particular manager.
-    # now lets count the total number of employees under a particular manager.
-    c = dict() # store manager:count of employee as key value
-    print(d)
-    
-if __name__=="__main__":
-  # t is tuple containing employee and boss pair.
-  t = (("A", "C"),("B", "C"),("C", "F"),("D", "E"),("E", "F"),("F", "F"))
-  obj = Solution()
-  obj.assignAndPrint(t)
-
