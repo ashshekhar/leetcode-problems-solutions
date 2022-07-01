70847cdba882e6e6c1bd75c95b1efbfb52c1bebe
ashshekhar <svs6603@psu.edu>
2022-07-01 16:34:29 +0530

    Finish problems

--- 5.longest-palindromic-substring.py
+++ 5.longest-palindromic-substring.py
@@ -11,12 +11,45 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
+        # Dynamic programming approach
         # Fill half matrix where dp[i, j] = 1 if s[i:j] is a palindrome, 0 otherwise
         # Next, fill the matrix for len >= 3
         # Keep track of max length palindrome while filling the dp table
         
         
+        # Expanding around centre approach
+        length = len(s)
         
+        global res_left, res_right
+        res_left, res_right = 0, 0
         
+        if length == 1:
+            return s[0]
+        
+        # Expand both ways till the max possible palindrome starting out from index (left, right)
+        def expandAroundCentre(left, right):
+            
+            global res_left, res_right
+            while left <= right and right < length and left >= 0 and s[left] == s[right]:
+                if right - left >= res_right - res_left:
+                    res_right = right
+                    res_left = left
+                    
+                left -= 1
+                right += 1     
+        
+        # The answer could be larger and odd or larger and even
+        for i in range(length):
+            
+            # If the largest palindrome is odd
+            left, right = i, i
+            expandAroundCentre(left, right)
+            
+            # If the largest palindrome is even
+            left, right = i,  i + 1
+            expandAroundCentre(left, right)
+            
+        return s[res_left: res_right + 1]
+           
 # @lc code=end
 
