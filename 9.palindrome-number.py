ebd0f60a2ff400a24880d3fff49199d1ab0b112b
ashshekhar <svs6603@psu.edu>
2022-07-01 23:36:50 +0530

    Finish problems

--- 9.palindrome-number.py
+++ 9.palindrome-number.py
@@ -1,12 +1,28 @@
 #
-# @lc app=leetcode id=9 lang=python3
+# @lc app=leetcode id=9 lang=python
 #
 # [9] Palindrome Number
 #
 
 # @lc code=start
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        return str(x) == str(x)[::-1]
-# @lc code=end
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        temp = x
+        right_read = 0
+        count = 0
 
+        while temp > 0:
+
+            digit = temp % 10
+        
+            right_read = right_read * 10 + digit
+            
+            temp = temp / 10
+
+        return x == right_read
+        # return str(x) == str(x)[::-1]
+# @lc code=end
