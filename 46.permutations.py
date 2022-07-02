2fdeb6a7e0b55855ec5c152da6ee94b49709d4f6
ashshekhar <svs6603@psu.edu>
2022-07-02 15:47:43 +0530

    Finish problems

--- 46.permutations.py
+++ 46.permutations.py
@@ -18,13 +18,21 @@ class Solution(object):
             return [nums[:]]
         
         # For each element, pop it and find all permutations of the remaining elements
+        
+        # When you return back for next iteration, the first element to be popped will now 
+        # be the second element in origianl list, because of the way we have written the function
+        
+        # [1, 2, 3] (pop 1) -> [2, 3] (pop 2) -> [3] (base case) -> [3, 2] (append back 2) and (pop 3) 
+        # -> [2] -> [2, 3] (add back 3) -> [2, 3, 1]; [3, 2, 1] (append back 1 for both the permutations)
+        
+        # Continue the same with [2, 3, 1] as your original array now
         for i in range(len(nums)):
             
             # Pop the element at index 0
             num = nums.pop(0)
             permutations = self.permute(nums)
             
-            # Add back the popped element in result
+            # Add back the element popped above in result
             for permutation in permutations:
                 permutation.append(num)
             
