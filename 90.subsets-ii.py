596f558ef2a4d1ecf3b56ddb3b1947a4e764a203
ashshekhar <svs6603@psu.edu>
2022-07-05 02:41:00 +0530

    Finish problems

--- 90.subsets-ii.py
+++ 90.subsets-ii.py
@@ -1,38 +1,41 @@
 #
-# @lc app=leetcode id=78 lang=python
+# @lc app=leetcode id=90 lang=python
 #
-# [78] Subsets
+# [90] Subsets II
 #
 
 # @lc code=start
 class Solution(object):
-    def subsets(self, nums):
+    def subsetsWithDup(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+
         res = []
         subset = []
         
-        # Draw decision tree to best understand
-        # Every index has two options - include or not
-        # Time: O(n * 2^n)
+        nums.sort()
         
         def backtrack(index):
             if index >= len(nums):
-                if subset not in res:
-                    res.append(subset[:])
+                res.append(subset[:])
                 return
             
             # Decision to add nums[index]
             subset.append(nums[index])
             backtrack(index + 1)
             
-            # Decision to not add nums[index]
+            # Decision to not add nums[index] and skip all next values which are equal to nums[index]
             subset.pop()
+            
+            while index + 1 < len(nums) and nums[index] == nums[index + 1]:
+                index += 1
+    
             backtrack(index + 1)
             
         backtrack(0)
         return res
+        
 # @lc code=end
 
