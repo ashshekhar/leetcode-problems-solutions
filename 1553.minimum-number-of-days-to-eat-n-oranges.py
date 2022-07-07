c02dd80611be04208f4fcc4172b0783d34f95e91
ashshekhar <svs6603@psu.edu>
2022-07-07 20:19:08 +0530

    Finish problems

--- 1553.minimum-number-of-days-to-eat-n-oranges.py
+++ 1553.minimum-number-of-days-to-eat-n-oranges.py
@@ -9,59 +9,59 @@ from collections import deque
 class Solution(object):
     
     # DP Solution
-    # def minDays(self, n):
-    #     """
-    #     :type n: int
-    #     :rtype: int
-    #     """
-    #     dp = {0: 0, 1: 1}
+    def minDays(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        dp = {0: 0, 1: 1}
 
-    #     def dfs(n):
-    #         if n in dp:
-    #             return dp[n]
+        def dfs(n):
+            if n in dp:
+                return dp[n]
             
-    #         # This would obv be the longest
-    #         # one = 1 + dfs(n - 1)
+            # This would obv be the longest
+            # one = 1 + dfs(n - 1)
             
-    #         # Instead, either go down / 2 path or -1 then /2
-    #         two = 1 + (n % 2) + dfs(n // 2)
+            # Instead, either go down / 2 path or -1 then /2
+            two = 1 + (n % 2) + dfs(n // 2)
             
-    #         # Or, either go down / 3 path or -1 then / 3
-    #         three = 1 + (n % 3) + dfs(n // 3)
+            # Or, either go down / 3 path or -1 then / 3
+            three = 1 + (n % 3) + dfs(n // 3)
             
-    #         # Store the shortest
-    #         dp[n] = min(two, three)
+            # Store the shortest
+            dp[n] = min(two, three)
 
-    #         return dp[n]
+            return dp[n]
             
-    #     return dfs(n)    
+        return dfs(n)    
     
     # BFS Solution
-    def minDays(self, n):
-        q = deque([n])
-        days = 0
-        visited = set()
+    # def minDays(self, n):
+    #     q = deque([n])
+    #     days = 0
+    #     visited = set()
         
-        while q:
-            days += 1
-            for _ in range(len(q)):
-                node = q.popleft()
+    #     while q:
+    #         days += 1
+    #         for _ in range(len(q)):
+    #             node = q.popleft()
                 
-                # Base case
-                if node == 1:
-                    return days
+    #             # Base case
+    #             if node == 1:
+    #                 return days
                 
-                if node - 1 not in visited:
-                    q.append(node - 1)
-                    visited.add(node - 1)
+    #             if node - 1 not in visited:
+    #                 q.append(node - 1)
+    #                 visited.add(node - 1)
                 
-                if node % 2 == 0 and node % 2 not in visited:
-                    q.append(node // 2)
-                    visited.add(node // 2)
+    #             if node % 2 == 0 and node % 2 not in visited:
+    #                 q.append(node // 2)
+    #                 visited.add(node // 2)
                     
-                if node % 3 == 0 and node % 3 not in visited:
-                    q.append(node // 3)
-                    visited.add(node // 3)
+    #             if node % 3 == 0 and node % 3 not in visited:
+    #                 q.append(node // 3)
+    #                 visited.add(node // 3)
                 
 # @lc code=end
 
