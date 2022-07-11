6d88a0fbf33df476f4ad3cf38aa003f7ee3f8893
ashshekhar <svs6603@psu.edu>
2022-07-11 10:57:52 +0530

    Finish problems

--- x.max-straight-line-drawing.py
+++ x.max-straight-line-drawing.py
@@ -18,27 +18,27 @@ for i in range(m-1, -1, -1):
 	for j in range(n-1, -1, -1):
 		if grid[i][j] == 1:
             
-            # At least one is possible
+      # At least one is possible
 			for k in range(4):
 				dp[i][j][k] = 1
             
-            # Case for bottom
+      # Case for bottom
 			if i+1 < m:
 				dp[i][j][0] += dp[i+1][j][0]
             
-            # Case for right
+      # Case for right
 			if j+1 < n:
 				dp[i][j][1] += dp[i][j+1][1]
             
-            # Case for right diagonal
+      # Case for right diagonal
 			if i+1 < m and j+1 < n:
 				dp[i][j][2] += dp[i+1][j+1][2]
                 
-            # Case for left diagonal
+      # Case for left diagonal
 			if i+1 < m and j-1 >= 0:	
 				dp[i][j][3] += dp[i+1][j-1][3]
             
-            # This is storing the max ever from the 4 vals but seems faulty
+      # This is storing the max ever from the 4 vals
 			ans = max(ans, max(dp[i][j]))
             
 for rows in dp:
