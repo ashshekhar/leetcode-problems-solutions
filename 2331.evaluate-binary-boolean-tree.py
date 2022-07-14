50b9efae6d4cb65243d4e8222c51ebbb97074548
ashshekhar <svs6603@psu.edu>
2022-07-14 20:38:48 +0530

    Finish problems

---
+++ 2331.evaluate-binary-boolean-tree.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def evaluateTree(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: bool
+        """
+        if not root:
+            return False
+        
+        if not root.left and not root.right:
+            return root.val != 0
+        
+        left_expr = self.evaluateTree(root.left)
+        right_expr = self.evaluateTree(root.right)
+        
+        if root.val == 2:
+            return (left_expr or right_expr)
+              
+        if root.val == 3:
+            return (left_expr and right_expr)
