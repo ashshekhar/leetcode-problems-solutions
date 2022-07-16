d13b501e1c05bd486933f36dce82136395b76089
ashshekhar <svs6603@psu.edu>
2022-07-17 00:53:01 +0530

    Finish problems

--- 42.trapping-rain-water.py
+++ 42.trapping-rain-water.py
@@ -20,12 +20,15 @@ class Solution(object):
         left_max[0] = 0
         right_max[length - 1] = 0
         
+        # The left max of each index, is the max of prev left_max and prev_height
         for i in range(1, length):
             left_max[i] = max(left_max[i-1], height[i-1])
         
+        # The right max of each index, is the max of next right_max and next_height
         for i in range(length - 2, -1, -1):
             right_max[i] = max(right_max[i+1], height[i+1])
         
+        # Find the result
         for i in range(length):
             diff = min(left_max[i], right_max[i]) - height[i]
             if diff > 0:
