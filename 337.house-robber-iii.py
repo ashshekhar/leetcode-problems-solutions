c02ba6f445cedc1095286706b9ec3e852196cc64
ashshekhar <svs6603@psu.edu>
2022-07-16 23:21:49 +0530

    Finish problems

--- 337.house-robber-iii.py
+++ 337.house-robber-iii.py
@@ -13,13 +13,18 @@
 #         self.right = right
 class Solution(object):
     def dfs(self, root):
+        
+        # Stores the max that can be robbed including [with_root, without_root]
         if not root:
             return [0, 0]
         
         left_pair = self.dfs(root.left)
         right_pair = self.dfs(root.right)
         
+        # Since you're including the root, need to take the without_root from both pairs
         with_root = root.val + left_pair[1] + right_pair[1]
+        
+        # Since not including the root, can take the max of both pairs
         without_root = max(left_pair) + max(right_pair)
         
         return [with_root, without_root]
