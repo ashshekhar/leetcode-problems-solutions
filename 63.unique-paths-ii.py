d4aabaabe3c0620cc4b532cde51d61ecc776b77f
ashshekhar <svs6603@psu.edu>
2022-07-18 19:29:45 +0530

    Finish problems

--- 63.unique-paths-ii.py
+++ 63.unique-paths-ii.py
@@ -12,6 +12,7 @@ class Solution(object):
         :rtype: int
         """
         # If initially blocked, return, else mark that as 1 number of ways to reach
+        # Visitable == 1 == 1 way to reach
         if obstacleGrid[0][0] == 1:
             return 0
         else:
@@ -20,14 +21,16 @@ class Solution(object):
         row = len(obstacleGrid)
         col = len(obstacleGrid[0])   
 
-        # Only if the prev cell is visitable (== 1) and current is (== 0) allowed to be visited, set it as 1 else 0
+        # Only if the prev cell is visitable (== 1) and current is (== 0) 
+        # allowed to be visited, set it as 1 else 0
         for i in range(1, row):
             obstacleGrid[i][0] = int(obstacleGrid[i-1][0] == 1 and obstacleGrid[i][0] == 0)
 
         for j in range(1, col):
             obstacleGrid[0][j] = int(obstacleGrid[0][j-1] == 1 and obstacleGrid[0][j] == 0)
         
-        # Finally, if you see a visitable cell, update its value else set it to 0, that is can't be visited or can be visited in 0 ways
+        # Finally, if you see a visitable cell, update its value else set it to 0, 
+        # that is can't be visited or can be visited in 0 ways
         for i in range(1, row):
             for j in range(1, col):
                 if obstacleGrid[i][j] == 0:
